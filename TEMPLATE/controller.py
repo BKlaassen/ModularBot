afb52da3ad4550060ef807f5196db38056eb1e3a
Bjarne Klaaßen <bjarnekam@web.de>
2019-04-06 21:50:36 +0200

    gotta do some checkout

--- TEMPLATE/controller.py
+++ TEMPLATE/controller.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     mp.set_start_method('spawn')
     database.load_database("controller")
     for element in getallfromdb("bots"):
-        bots.update(element['name'])
+        bots.update(element['name']=[element['folder']])
