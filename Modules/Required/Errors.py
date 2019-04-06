a1418011d79c96283097d0928d3c1ff77d2e96ae
Bjarne Klaaßen <bjarnekam@web.de>
2019-04-06 21:45:26 +0200

    gotta do some checkout

--- Modules/Required/Errors.py
+++ Modules/Required/Errors.py
@@ -5,3 +5,11 @@ class InsufficientParameterException(Exception):
 
     def __str__(self):
         return "Command %s called with insufficient Parameters" % command
+
+class UnexpectedBotClosure(Exception):
+    """One Bot Instance Unexpectedly close"""
+    def init(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "Bot for Channel %s unexpectedly closed" % self.name
---
+++ TEMPLATE/controller.py
@@ -0,0 +1,11 @@
+from ModularBot import bot
+from Modules.Required import Database as database
+import multiprocessing
+
+bots = dict()
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    database.load_database("controller")
+    for element in getallfromdb("bots"):
+        bots.update(element['name'])
